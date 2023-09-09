23de7e11c738d0978fcb4e775d3f363620572b2a
RouaHamdi <139374520+RouaHamdi@users.noreply.github.com>
2023-09-09 16:01:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+return (
+    <div className="App">
+    <header className="App-header">
+    <>
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+        </Navbar>
+        <br />
+        <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+        </Navbar>
+
+        <br />
+        <Navbar bg="light" data-bs-theme="light">
+        <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+        </Navbar>
+    </>
+
+    </header>
+    </div>
+);
+}
+
+export default App;
+
+
+
+
